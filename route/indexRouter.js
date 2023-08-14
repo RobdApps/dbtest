f3a16f8432f983be3d3099c86ce3ddca240b8cef
RobdApps <122159043+RobdApps@users.noreply.github.com>
2023-08-14 21:21:07 +0300

    Delete route directory

--- route/indexRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get("/", (req, res)=> {
-    res.json({"status": "OK"})
-    console.log("index at", new Date, "from", req.ip)
-})
-
-exports.router = router
