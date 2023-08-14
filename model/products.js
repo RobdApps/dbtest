524e3a8ee5cbbd5eed84b76989c7735f550ae4eb
RobdApps <122159043+RobdApps@users.noreply.github.com>
2023-08-14 21:51:10 +0300

    Update products.js

--- model/products.js
+++ model/products.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const productSchema = new Schema({
+    title: {type: String, required: true},
+    description: String,
+    price: {type: Number, required: true},
+    discountPercentage: mongoose.Types.Decimal128,
+    rating: mongoose.Types.Decimal128,
+    stock: Number,
+    brand: {type: String, required: true},
+    category: String,
     images: [ String ]
 })
 
